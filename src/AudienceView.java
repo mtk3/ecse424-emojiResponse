2beb5f32dc24695ce26b77f3f9b36e736518de55
mtk3 <mitchell.keeley@videotron.ca>
2020-11-16 17:45:00 -0500

    fixed colors, diverse classmates, updated .jar

--- src/AudienceView.java
+++ src/AudienceView.java
@@ -47,8 +47,6 @@ public class AudienceView extends JFrame {
 		
 		// background setup
 		this.setSize(screenSize);
-		//this.setPreferredSize(new Dimension(1024, 768));
-		//getContentPane().setPreferredSize(new Dimension(1024, 768));
 		getContentPane().setBackground(Color.WHITE);
 		
 		// screenshare image
@@ -77,7 +75,7 @@ public class AudienceView extends JFrame {
 		Top.add(person1Label);
 		
 		// Classmate images
-		BufferedImage person2 = ImageIO.read(new File(gender + emotion + skinToneNum));
+		BufferedImage person2 = ImageIO.read(new File("./images/emoji/man/default/person0.png"));
 		Image scaledPerson2 = person2.getScaledInstance(width/9, height/7, Image.SCALE_SMOOTH);
 		ImageIcon person2Icon = new ImageIcon(scaledPerson2);
 		JLabel person2Label = new JLabel();
@@ -88,7 +86,7 @@ public class AudienceView extends JFrame {
 		person2Label.setPreferredSize(new Dimension(width/8, height/6));
 		Top.add(person2Label);
 		
-		BufferedImage person3 = ImageIO.read(new File(gender + emotion + skinToneNum));
+		BufferedImage person3 = ImageIO.read(new File("./images/emoji/person/default/person3.png"));
 		Image scaledPerson3 = person3.getScaledInstance(width/9, height/7, Image.SCALE_SMOOTH);
 		ImageIcon person3Icon = new ImageIcon(scaledPerson3);
 		JLabel person3Label = new JLabel();
@@ -99,7 +97,7 @@ public class AudienceView extends JFrame {
 		person3Label.setPreferredSize(new Dimension(width/8, height/6));
 		Top.add(person3Label);
 		
-		BufferedImage person4 = ImageIO.read(new File(gender + emotion + skinToneNum));
+		BufferedImage person4 = ImageIO.read(new File("./images/emoji/woman/bored/bored1.png"));
 		Image scaledPerson4 = person4.getScaledInstance(width/9, height/7, Image.SCALE_SMOOTH);
 		ImageIcon person4Icon = new ImageIcon(scaledPerson4);
 		JLabel person4Label = new JLabel();
@@ -110,7 +108,7 @@ public class AudienceView extends JFrame {
 		person4Label.setPreferredSize(new Dimension(width/8, height/6));
 		Top.add(person4Label);
 		
-		BufferedImage person5 = ImageIO.read(new File(gender + emotion + skinToneNum));
+		BufferedImage person5 = ImageIO.read(new File("./images/emoji/woman/question/question5.png"));
 		Image scaledPerson5 = person5.getScaledInstance(width/9, height/7, Image.SCALE_SMOOTH);
 		ImageIcon person5Icon = new ImageIcon(scaledPerson5);
 		JLabel person5Label = new JLabel();
@@ -121,7 +119,7 @@ public class AudienceView extends JFrame {
 		person5Label.setPreferredSize(new Dimension(width/8, height/6));
 		Top.add(person5Label);
 		
-		BufferedImage person6 = ImageIO.read(new File(gender + emotion + skinToneNum));
+		BufferedImage person6 = ImageIO.read(new File("./images/emoji/woman/louder/louder1.png"));
 		Image scaledPerson6 = person6.getScaledInstance(width/9, height/7, Image.SCALE_SMOOTH);
 		ImageIcon person6Icon = new ImageIcon(scaledPerson6);
 		JLabel person6Label = new JLabel();
@@ -132,7 +130,7 @@ public class AudienceView extends JFrame {
 		person6Label.setPreferredSize(new Dimension(width/8, height/6));
 		Top.add(person6Label);
 		
-		BufferedImage person7 = ImageIO.read(new File(gender + emotion + skinToneNum));
+		BufferedImage person7 = ImageIO.read(new File("./images/emoji/man/stressed/stressed2.png"));
 		Image scaledPerson7 = person7.getScaledInstance(width/9, height/7, Image.SCALE_SMOOTH);
 		ImageIcon person7Icon = new ImageIcon(scaledPerson7);
 		JLabel person7Label = new JLabel();
@@ -143,7 +141,7 @@ public class AudienceView extends JFrame {
 		person7Label.setPreferredSize(new Dimension(width/8, height/6));
 		Top.add(person7Label);
 		
-		BufferedImage person8 = ImageIO.read(new File(gender + emotion + skinToneNum));
+		BufferedImage person8 = ImageIO.read(new File("./images/emoji/man/confused/shrugging4.png"));
 		Image scaledPerson8 = person8.getScaledInstance(width/9, height/7, Image.SCALE_SMOOTH);
 		ImageIcon person8Icon = new ImageIcon(scaledPerson8);
 		JLabel person8Label = new JLabel();
@@ -164,7 +162,7 @@ public class AudienceView extends JFrame {
 		
 		// emoji selection pane
 		JLayeredPane emojiPane = new JLayeredPane();
-		emojiPane.setBackground(new Color(102, 255, 0, 255));
+		emojiPane.setBackground(new Color(102, 204, 255, 255));
 		emojiPane.setPreferredSize(new Dimension(width, height/6));
 		Bottom.add(emojiPane);
 		emojiPane.setLayout(new CardLayout(0, 0));
@@ -173,7 +171,7 @@ public class AudienceView extends JFrame {
 		
 		// gender selection pane
 		JPanel genderPane = new JPanel(new FlowLayout(FlowLayout.LEFT, 0, 0));
-		genderPane.setBackground(new Color(255, 0, 255, 255));
+		genderPane.setBackground(new Color(102, 204, 255, 255));
 		genderPane.setPreferredSize(new Dimension(width, height/6));
 		emojiPane.add(genderPane, "genderPane");
 		genderPane.setVisible(emojiVis);
@@ -232,7 +230,7 @@ public class AudienceView extends JFrame {
 		
 		// skin tone selection pane
 		JPanel skinTonePane = new JPanel(new FlowLayout(FlowLayout.LEFT, 0, 0));
-		skinTonePane.setBackground(new Color(0, 255, 255, 255));
+		skinTonePane.setBackground(new Color(102, 204, 255, 255));
 		skinTonePane.setPreferredSize(new Dimension(width, height/6));
 		emojiPane.add(skinTonePane, "skinTonePane");
 		skinTonePane.setVisible(emojiVis);
@@ -342,7 +340,7 @@ public class AudienceView extends JFrame {
 		
 		// emotion selection pane
 		JPanel emotionPane = new JPanel(new FlowLayout(FlowLayout.LEFT, 0, 0));
-		emotionPane.setBackground(Color.RED);
+		emotionPane.setBackground(new Color(102, 204, 255, 255));
 		emotionPane.setPreferredSize(new Dimension(width, height/6));
 		emojiPane.add(emotionPane, "emotionPane");
 		//emojiPane.moveToFront(emotionPane);
